c6d51ab1d7821a8878c95975f466dda822c18f23
JOSE GUTIERREZ LEWIS <josefran952009@gmail.com>
2023-06-21 07:52:27 -0600

    Reponsive login

--- Tienda/Productos/Productos.aspx.designer.cs
+++ Tienda/Productos/Productos.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace Tienda.Productos
 
     public partial class Productos
     {
+
+        /// <summary>
+        /// Control DataListRopa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataListRopa;
+
+        /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
     }
 }
