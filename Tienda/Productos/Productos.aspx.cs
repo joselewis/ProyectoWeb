55dc2491590cd306348d497ebc6cc0b23a449a66
JOSE GUTIERREZ LEWIS <josefran952009@gmail.com>
2023-06-21 07:33:20 -0600

    Prueba de commit

--- Tienda/Productos/Productos.aspx.cs
+++ Tienda/Productos/Productos.aspx.cs
@@ -12,22 +12,22 @@ namespace Tienda.Productos
 {
     public partial class Productos : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            SqlConnection con = new SqlConnection(@"DATA SOURCE = JOSELEWIS; INITIAL CATALOG = TIENDA_VIERNES; USER = JoseLewis10; PASSWORD = joselewis10");
-            con.Open();
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    SqlConnection con = new SqlConnection(@"DATA SOURCE = JOSELEWIS; INITIAL CATALOG = TIENDA_VIERNES; USER = JoseLewis10; PASSWORD = joselewis10");
+        //    con.Open();
 
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = System.Data.CommandType.Text;
-            cmd.CommandText = "SELECT * FROM PRODUCTO_ROPA";
-            cmd.ExecuteNonQuery();
+        //    SqlCommand cmd = con.CreateCommand();
+        //    cmd.CommandType = System.Data.CommandType.Text;
+        //    cmd.CommandText = "SELECT * FROM PRODUCTO_ROPA";
+        //    cmd.ExecuteNonQuery();
 
-            DataTable dt = new DataTable();
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            adapter.Fill(dt);
-            DataListRopa.DataSource = dt;
-            DataListRopa.DataBind();
-            con.Close();
-        }
+        //    DataTable dt = new DataTable();
+        //    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        //    adapter.Fill(dt);
+        //    DataListRopa.DataSource = dt;
+        //    DataListRopa.DataBind();
+        //    con.Close();
+        //}
     }
 }
--- Tienda/Productos/Productos.aspx.designer.cs
+++ Tienda/Productos/Productos.aspx.designer.cs
@@ -13,23 +13,5 @@ namespace Tienda.Productos
 
     public partial class Productos
     {
-
-        /// <summary>
-        /// Control DataListRopa.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DataList DataListRopa;
-
-        /// <summary>
-        /// Control lblError.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblError;
     }
 }
