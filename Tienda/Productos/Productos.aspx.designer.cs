4dc63f61be4cbe68daefc09a825cf34e83f6ed5a
JOSE GUTIERREZ LEWIS <josefran952009@gmail.com>
2023-06-25 17:29:09 -0600

    Mejora a la vista de todos los productos

--- Tienda/Productos/Productos.aspx.designer.cs
+++ Tienda/Productos/Productos.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace Tienda.Productos
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataList DataListRopa;
-
-        /// <summary>
-        /// Control lblError.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblError;
     }
 }
