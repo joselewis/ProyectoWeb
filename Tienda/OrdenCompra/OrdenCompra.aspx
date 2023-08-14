825637815156fa7e9c158193926e9353614c8f29
JOSE GUTIERREZ LEWIS <josefran952009@gmail.com>
2023-08-14 15:48:56 -0600

    Inicio de la orden de compra

--- Tienda/OrdenCompra/OrdenCompra.aspx
+++ Tienda/OrdenCompra/OrdenCompra.aspx
@@ -2,4 +2,25 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<center>
+    <div style="col">
+    <asp:Label ID="LblId" runat="server" Visible="true" Text="Id Orden Compra"></asp:Label>
+    </div> 
+    <div style="col">
+        <asp:Label ID="LblCorreo" runat="server" Visible="true" Text="Correo"></asp:Label>
+    </div> 
+    <div style="col">
+        <asp:Label ID="LblCodigoProducto" runat="server" Visible="true" Text="Código Producto"></asp:Label>
+    </div> 
+    <div style="col">
+        <asp:Label ID="LblIdCarrito" runat="server" Visible="true" Text="Id Carrito"></asp:Label>
+    </div> 
+    <div style="col">
+        <asp:Label ID="LblMetodoPago" runat="server" Visible="true" Text="Método Pago"></asp:Label>
+    </div> 
+    <div style="col">
+        <asp:Label ID="LblTotalPago" runat="server" Visible="true" Text="Total a Pagar"></asp:Label>
+    </div> 
+</center>
+<br />
 </asp:Content>
--- Tienda/OrdenCompra/OrdenCompra.aspx.designer.cs
+++ Tienda/OrdenCompra/OrdenCompra.aspx.designer.cs
@@ -13,5 +13,59 @@ namespace Tienda.PagoFinal
 
     public partial class OrdenCompra
     {
+
+        /// <summary>
+        /// Control LblId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblId;
+
+        /// <summary>
+        /// Control LblCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblCorreo;
+
+        /// <summary>
+        /// Control LblCodigoProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblCodigoProducto;
+
+        /// <summary>
+        /// Control LblIdCarrito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblIdCarrito;
+
+        /// <summary>
+        /// Control LblMetodoPago.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblMetodoPago;
+
+        /// <summary>
+        /// Control LblTotalPago.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblTotalPago;
     }
 }
